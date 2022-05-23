c5754474de2ad5024517ee86864e048f536ab18d
wgpdb <dariusz.borek@gmail.com>
2022-05-23 09:35:08 +0200

    24.3 First commit

---
+++ src/main/java/com/kodilla/patterns2/facade/OrderWatcher.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns2.facade;
+
+import com.kodilla.patterns2.facade.api.OrderDto;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class OrderWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, userId) && target(object)")
+    public void logEvent(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Order process executed. Class: " + object.getClass().getName() + " Args: " + "order size: " +
+                order.getItems().size() + ", userId: " + userId);
+    }
+}
